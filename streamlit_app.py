6943b85dd81ff06444501df12828d9473b4ba128
tomurhosnowflake <126225735+tomurhosnowflake@users.noreply.github.com>
2023-02-24 17:41:09 +0800

    Update streamlit_app.py
    Added code to format the API call

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,3 +31,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# normalizes the content
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# displays in a table
+streamlit.dataframe(fruityvice_normalized)
