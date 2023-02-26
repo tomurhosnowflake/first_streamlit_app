e4b9a2c7ee8163314d9cdceec44f6582733b14b0
tomurhosnowflake <126225735+tomurhosnowflake@users.noreply.github.com>
2023-02-26 17:57:28 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ def get_fruityvice_data(this_fruit_choice):
   return fruityvice_normalized
 
 # New section to display fruitvice api response
-streamlit.header("Fruityvice Fruit Advice!")
+streamlit.header('Fruityvice Fruit Advice!')
 
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
